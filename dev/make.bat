329390f940e4479b957090c5f4a7a4c01f143b15
gnawhleinad <danielleehwang@gmail.com>
2014-12-08 16:52:34 -0800

    add make.bat

---
+++ dev/make.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if "%1" == "sums" (
+	vagrant up && vagrant ssh --command "/ohana/dev/calculate_sums.sh"
+	if errorlevel 1 exit /b 1
+	goto end
+)
+
+if "%1" == "clean" (
+	vagrant destroy --force
+	if errorlevel 1 exit /b 1
+	goto end
+)
+
+:end
